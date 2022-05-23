8342fee1ebcd244a10eafdb9799f118d1a69c16a
ChamzyK <ChamzyK@yandex.ru>
2022-05-24 02:49:48 +0700

    IoC

--- ReportCompiler.WPF/App.xaml.cs
+++ ReportCompiler.WPF/App.xaml.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
 
 namespace ReportCompiler.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static IHost? host;
+        public static IHost Host => 
+            host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs())
+            .Build();
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+
+            base.OnStartup(e);
+
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            using (Host) await Host.StopAsync();
+        }
     }
 }
