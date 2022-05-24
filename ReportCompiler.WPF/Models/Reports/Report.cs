679d00b7065cb0fe5cf15a24fc75f41f36e7f7b4
ChamzyK <ChamzyK@yandex.ru>
2022-05-25 05:06:27 +0700

    Добавлены модели для отчетов.

---
+++ ReportCompiler.WPF/Models/Reports/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace ReportCompiler.WPF.Models.Reports
+{
+    internal class Report
+    {
+        public string Month { get; set; }
+        public string District { get; set; }
+        public Table TableData { get; set; }
+    }
+}
