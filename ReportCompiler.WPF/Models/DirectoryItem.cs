06863ef37bbf17b20763621b48a5c52a0ccb4864
ChamzyK <ChamzyK@yandex.ru>
2022-05-24 14:18:29 +0700

    Добавлена иконка для приложения, а также исправлено отображение файлов Excel.

--- ReportCompiler.WPF/Models/DirectoryItem.cs
+++ ReportCompiler.WPF/Models/DirectoryItem.cs
@@ -1,8 +1,11 @@
-﻿namespace ReportCompiler.WPF.Models
+﻿using MaterialDesignThemes.Wpf;
+
+namespace ReportCompiler.WPF.Models
 {
     internal class DirectoryItem
     {
         public string? Name { get; set; }
         public bool IsDirectory { get; set; }
+        public PackIconKind PackIconKind => IsDirectory ? PackIconKind.Folder : PackIconKind.MicrosoftExcel;
     }
 }
