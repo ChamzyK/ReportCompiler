f6d1204bfb867a7d939724e43792dd2d5888c6eb
ChamzyK <ChamzyK@yandex.ru>
2022-05-25 03:51:18 +0700

    Добавлена возможность возвращения к исходной папке в директориях.

---
+++ ReportCompiler.WPF/Infrastructure/Commands/AppDirectoryCommand.cs
@@ -0,0 +1,22 @@
+﻿using ReportCompiler.WPF.Infrastructure.Commands.Base;
+using ReportCompiler.WPF.Services.Interfaces;
+using ReportCompiler.WPF.ViewModels.UserControlViewModels;
+
+namespace ReportCompiler.WPF.Infrastructure.Commands
+{
+    internal class AppDirectoryCommand : Command
+    {
+        protected override void Execute(object? parameter)
+        {
+            if (App.Services.GetService(typeof(DirectoriesViewModel)) == null
+                || App.Services.GetService(typeof(IDirectory)) == null) return;
+
+            if (App.Services.GetService(typeof(DirectoriesViewModel)) is not DirectoriesViewModel dvm 
+                || dvm.DirectoryService == null) return;
+
+
+            var directoryService = App.Services.GetService(typeof(IDirectory)) as IDirectory;
+            dvm.DirectoryService = directoryService;
+        }
+    }
+}
--- ReportCompiler.WPF/Services/ServicesRegistrator.cs
+++ ReportCompiler.WPF/Services/ServicesRegistrator.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ReportCompiler.WPF.Models;
 using ReportCompiler.WPF.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -16,7 +17,7 @@ namespace ReportCompiler.WPF.Services
             .AddTransient<ICollection<DirectoryItem>, ObservableCollection<DirectoryItem>>()
             .AddTransient(services =>
             {
-                var currDir = Directory.GetCurrentDirectory();
+                var currDir = Environment.CurrentDirectory;
                 var currDirInfo = new DirectoryInfo(currDir);
                 return new DirectoryItem(currDirInfo.Name, currDirInfo.FullName, DirectoryItemType.Directory);
             })
