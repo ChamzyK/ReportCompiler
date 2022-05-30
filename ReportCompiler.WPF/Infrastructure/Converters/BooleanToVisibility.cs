4b2ddae4de8be57342a048792f8c7d1afe6aaf12
ChamzyK <ChamzyK@yandex.ru>
2022-05-30 14:13:43 +0700

    Добавлены новые элементы в интерфейсе окна.

---
+++ ReportCompiler.WPF/Infrastructure/Converters/BooleanToVisibility.cs
@@ -0,0 +1,19 @@
+﻿using ReportCompiler.WPF.Infrastructure.Converters.Base;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ReportCompiler.WPF.Infrastructure.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    internal class BooleanToVisibility : Converter
+    {
+        public override object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool type) return null;
+
+            return type ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
